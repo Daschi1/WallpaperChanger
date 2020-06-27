1612282c0c52d6a3fa183a4f0a2320734b738c1a
Daschi1 <50054971+Daschi1@users.noreply.github.com>
2020-06-27 22:23:36 +0200

    Added Core

--- src/main/java/de/daschi/main/Main.java
+++ src/main/java/de/daschi/main/Main.java
@@ -1,11 +1,79 @@
 package de.daschi.main;
 
 import de.daschi.core.Core;
+import org.simpleyaml.configuration.file.YamlConfiguration;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Main {
-    public static void main(final String[] args) {
-        Core.changeWallpaper(new File("r0np83qkcd751.jpg"));
+
+    private static TrayIcon trayIcon;
+    private static final MenuItem title = new MenuItem("");
+    private static final MenuItem ignoreHorizontal = new MenuItem("");
+    private static final MenuItem nextImage = new MenuItem("");
+
+    public static void main(final String[] args) throws IOException {
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                Core.saveCurrentHourImages();
+                Core.setIndex(-1);
+                Core.nextImage();
+                Core.downloadHourImage(Core.getIndex());
+                Core.changeWallpaper(new File("WallpaperChanger\\image.jpg"));
+                try {
+                    Main.updateSystemTray();
+                } catch (final IOException exception) {
+                    exception.printStackTrace();
+                }
+            }
+        }, 0, 1000 * 60 * 60);
+
+        if (SystemTray.isSupported()) {
+            final SystemTray systemTray = SystemTray.getSystemTray();
+            final BufferedImage bufferedImage = ImageIO.read(new File("WallpaperChanger\\image.jpg"));
+
+            final PopupMenu popup = new PopupMenu();
+            popup.add(Main.title);
+            Main.ignoreHorizontal.addActionListener(e -> {
+                Core.setIgnoreHorizontal(!Core.isIgnoreHorizontal());
+                try {
+                    Main.updateSystemTray();
+                } catch (final IOException exception) {
+                    exception.printStackTrace();
+                }
+            });
+            popup.add(Main.ignoreHorizontal);
+            Main.nextImage.addActionListener(e -> Core.nextImage());
+            popup.add(Main.nextImage);
+            final MenuItem exit = new MenuItem("Exit");
+            exit.addActionListener(e -> System.exit(0));
+            popup.add(exit);
+
+            Main.trayIcon = new TrayIcon(bufferedImage, "WallpaperChanger", popup);
+            Main.trayIcon.setImageAutoSize(true);
+            try {
+                systemTray.add(Main.trayIcon);
+            } catch (final AWTException exception) {
+                exception.printStackTrace();
+            }
+        }
+
+        Main.updateSystemTray();
+    }
+
+    public static void updateSystemTray() throws IOException {
+        final String saveUrl = Core.getHourSaveUrl();
+        final YamlConfiguration yamlConfiguration = YamlConfiguration.loadConfiguration(new File("WallpaperChanger\\config.yml"));
+        Main.trayIcon.setImage(ImageIO.read(new File("WallpaperChanger\\image.jpg")));
+        Main.title.setLabel(yamlConfiguration.getString(saveUrl + Core.getIndex() + ".name"));
+        Main.ignoreHorizontal.setLabel(Core.isIgnoreHorizontal() ? "Ignore horizontal - on" : "Ignore horizontal - off");
+        Main.nextImage.setLabel("Next image (Currently number " + Core.getIndex() + ")");
     }
 }
